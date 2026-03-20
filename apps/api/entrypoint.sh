2d62da71309d0d23c381701d0134efb17b073b28
jaeukkang77 <hello@luenn.xyz>
2026-03-21 01:16:47 +0900

    fix: push 마다 seed.ts 실행되던 에러 수정

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -4,8 +4,5 @@ set -e
 echo "[API] Running database migrations..."
 /app/node_modules/.bin/prisma migrate deploy --schema=/app/packages/prisma-db/prisma/schema.prisma
 
-echo "[API] Running database seed..."
-/app/node_modules/.bin/tsx /app/packages/prisma-db/prisma/seed.ts
-
 echo "[API] Starting server..."
 exec node /app/apps/api/dist/main.js
